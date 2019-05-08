a8ec5a09ef9759173c781a9fb7d49f8e4b435de4
Preda Alexandru <alexpreda95@yahoo.com>
2019-05-08 19:04:30 +0300

    Revert "Deleted files"
    This reverts commit a479c3f710482fda6c00f0c7b7d7be666be40f89.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,20 +5,63 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
-
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
+     /**
+     * @Route("/contact", name="form_contact")
+     */
+    public function pagContactAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class)
+            ->add('message', TextareaType::class)
+            ->add('send', SubmitType::class)
+            ->getForm();   
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $data = $form->getData();
+            
+            dump($data);
+            
+            $content = "<strong>Email:</strong> " . $form->getData()['email'] . "<br>"
+                . "<strong>Message:</strong> " . $form->getData()['message'] . "<br>";
+            
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Mesaj contact nou')
+                ->setFrom($form->getData()['email'])
+                ->setTo($this->getParameter('mailer_to'))
+                ->setBody($content);
+            
+            $this->get('mailer')->send($message);
+        }
+        
+         return $this->render('Alex/contact.html.twig', [
+             'my_form' => $form->createView(),
+         ]);
+    }
     
     /**
-     * @Route("/", name="")
+     * @Route("/", name="pagina_personala")
      */
-    public function pagAction(Request $request)
+    public function pagPersonalaAction(Request $request)
     {
       
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig');
+        return $this->render('Alex/alex.html.twig');
     }
-
+    
+    /**
+     * @Route("/alexandru", name="pagina_statica")
+     */
+    public function pagStaticAction(Request $request)
+    {
+        die('Salutare! Sunt Preda Alexandru');
+    }
 }
