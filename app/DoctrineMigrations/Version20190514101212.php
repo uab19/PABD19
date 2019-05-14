d2bc2a4afa6484395c616fb3e4d111ab7c42a242
Oancea Maria Nicoleta <oanceamarianicoleta@gmail.com>
2019-05-14 17:41:14 +0300

    Modificare fisier de migrare

--- app/DoctrineMigrations/Version20190514101212.php
+++ app/DoctrineMigrations/Version20190514101212.php
@@ -17,8 +17,7 @@ final class Version20190514101212 extends AbstractMigration
 
         $this->addSql('CREATE TABLE blog_post (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(100) NOT NULL, image VARCHAR(100) NOT NULL, content TEXT NOT NULL, user_id INT NOT NULL, create_date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE blog_post_comment (id INT AUTO_INCREMENT NOT NULL, blog_post_id INT DEFAULT NULL, user_id INT DEFAULT NULL, message TEXT NOT NULL, create_date DATETIME NOT NULL, INDEX blog_post_comment_blog_post_id_fk (blog_post_id), INDEX blog_post_comment_user_id_fk (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE gallery (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, file LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE blog_post_comment ADD CONSTRAINT FK_F3400AD8A77FBEAF FOREIGN KEY (blog_post_id) REFERENCES blog_post (id)');
+       $this->addSql('ALTER TABLE blog_post_comment ADD CONSTRAINT FK_F3400AD8A77FBEAF FOREIGN KEY (blog_post_id) REFERENCES blog_post (id)');
         $this->addSql('ALTER TABLE blog_post_comment ADD CONSTRAINT FK_F3400AD8A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
     }
 
@@ -31,6 +30,5 @@ final class Version20190514101212 extends AbstractMigration
         $this->addSql('ALTER TABLE blog_post_comment DROP FOREIGN KEY FK_F3400AD8A76ED395');
         $this->addSql('DROP TABLE blog_post');
         $this->addSql('DROP TABLE blog_post_comment');
-        $this->addSql('DROP TABLE gallery');
     }
 }
