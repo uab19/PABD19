769c019aeae9b55ed49bf340fb5c5f5b9aa4f1c1
mitzaalex <mitza.al@gmail.com>
2019-05-14 04:12:43 +0300

    front page with dynamic generated personal page links

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,11 +14,39 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-      
+
+        $personalStatiRoutes = $this->getStaticRoutesWithImages();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'routes' => $personalStatiRoutes
         ]);
+    }
+
+    private function getStaticRoutesWithImages() {
+        $routes = [];
+
+        $router = $this->container->get('router');
+        /* @var $router \Symfony\Component\Routing\Router */
+
+        $collection = $router->getRouteCollection();
+
+        $allRoutes = $collection->all();
+
+        foreach ($allRoutes as $key => $route)
+        {
+            if (isset($route->getDefaults()['img']) === true) {
+                $staticRoute = [];
+                $staticRoute['name'] = $key;
+                $staticRoute['path'] = $route->getPath();
+                $staticRoute['image'] = $route->getDefaults()['img'];
+
+                $routes[] = $staticRoute;
+            }
+        }
+
+        return $routes;
     }
     
 }
